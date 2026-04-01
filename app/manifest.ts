ead020b14404206c019a79737ec2d9b90b23c1e7
Justus Hamkens <justushamkens@Mini-von-Justus.fritz.box>
2026-04-01 19:15:53 +0200

    PWA: Android-Icons im Manifest korrekt hinterlegen

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,12 +13,18 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait",
     icons: [
       {
-        src: "/icon",
+        src: "/pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/pwa-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
       {
-        src: "/apple-icon",
+        src: "/apple-touch-icon.png",
         sizes: "180x180",
         type: "image/png",
       },
